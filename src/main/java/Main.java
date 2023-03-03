c20d834bc0c21ba380f41d108f924173f87fe302
Nora <nora@Zipcoders-MacBook-Pro-51.local>
2023-03-03 16:18:08 -0500

    Answered

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Input your name: ");
+        String userName = userInput.nextLine();
 
+        if (userName.equals("Alice") || userName.equals("Bob")) {
+            System.out.println("Hello " + userName + ".");
+        }
+        else {
+
+        }
     }
 }
